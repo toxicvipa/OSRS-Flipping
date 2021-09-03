5210d646f90fb1d766a6e7caecafd3934e11b706
toxicvipa <robin.tretter@gmail.com>
2021-09-03 15:20:02 +0200

    added bash file to run python file

---
+++ request.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+set /p min_percentage=Minimum profit percentage:
+set /p members=Include Members items? (y/n):
+set /p min_vol=Minimum volume[enter to skip]:
+set /p min_margin=Minimum margin[enter to skip]:
+if "%members%" == "y" set members=true
+if not "%members%" == "true" set members=false
+if "%min_vol%" == "" set min_vol=0
+if "%min_margin%" == "" set min_vol=0
+python flipper.py %min_percentage% %members% %min_vol% %min_margin%
+pause

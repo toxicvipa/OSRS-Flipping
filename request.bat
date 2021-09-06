5804c3f2b50b900a224b3fc9f85926c0100b2631
toxicvipa <robin.tretter@gmail.com>
2021-09-06 13:11:00 +0200

    added reload for script

--- request.bat
+++ request.bat
@@ -8,5 +8,8 @@ if "%members%" == "y" set members=true
 if not "%members%" == "true" set members=false
 if "%min_vol%" == "" set min_vol=0
 if "%min_margin%" == "" set min_vol=0
+:a
+cls
 python flipper.py %min_percentage% %members% %min_vol% %min_margin%
 pause
+goto a
